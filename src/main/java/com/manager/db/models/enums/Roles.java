ab1306a9b6e9ce369dc3cd059a41d4c6f564dc7a
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-22 09:01:47 -0500

    feat(enums): add Roles and Status to user

---
+++ src/main/java/com/manager/db/models/enums/Roles.java
@@ -0,0 +1,6 @@
+package com.manager.db.models.enums;
+
+public enum Roles {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/manager/db/models/enums/Status.java
@@ -0,0 +1,6 @@
+package com.manager.db.models.enums;
+
+public enum Status {
+    ACTIVATE,
+    BLOCKED
+}
