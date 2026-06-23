d1f318aa73d6d83200b5d01cd80f38f7085b7607
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-22 23:56:46 -0500

    feat(UserController): add views to display users database managment

--- src/main/java/com/manager/db/controllers/UserController.java
+++ src/main/java/com/manager/db/controllers/UserController.java
@@ -1,4 +1,82 @@
 package com.manager.db.controllers;
 
+import com.manager.db.models.Database;
+import com.manager.db.models.User;
+import com.manager.db.services.DatabaseService;
+import com.manager.db.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/dashboard")
 public class UserController {
+
+    private final DatabaseService databaseService;
+    private final UserService userService;
+
+    public UserController(DatabaseService databaseService, UserService userService) {
+        this.databaseService = databaseService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String showDashboard(Principal principal, Model model) {
+
+        String email = principal.getName();
+
+        User userLogged = userService.getByNameOrEmail(email).get(0);
+
+        List<Database> myDbs = databaseService.getDatabase(userLogged.getId());
+
+        model.addAttribute("databases", myDbs);
+        model.addAttribute("user", userLogged);
+
+        return "templates/private/dashboard";
+
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+
+        model.addAttribute("database", new Database());
+
+        return "templates/private/create-db";
+
+    }
+
+    @PostMapping("/new")
+    public String createDatabase(@ModelAttribute("database") Database database, Principal principal, Model model) {
+
+        try {
+
+            String email = principal.getName();
+
+            User userLogged = userService.getByNameOrEmail(email).get(0);
+
+            databaseService.save(userLogged.getId(), database);
+
+            return "redirect:/dashboard?success=created";
+
+        } catch (Exception e) {
+
+            model.addAttribute("error", e.getMessage());
+
+            return "templates/private/create-db";
+
+        }
+    }
+
+    @PostMapping("/{id}/archive")
+    public String archive(@PathVariable("id") UUID id) {
+
+        databaseService.archiveDatabase(id);
+
+        return "redirect:/dashboard?success=archived";
+
+    }
 }
