e137cccee59e13705bfa4b4b4a890913046207a3
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-22 23:04:11 -0500

    feat(DatabaseRepository): add 3 methods to find databases in repository

--- src/main/java/com/manager/db/repositories/DatabaseRepository.java
+++ src/main/java/com/manager/db/repositories/DatabaseRepository.java
@@ -1,9 +1,17 @@
 package com.manager.db.repositories;
 
 import com.manager.db.entities.DatabaseEntity;
+import com.manager.db.enums.DatabaseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DatabaseRepository extends JpaRepository<DatabaseEntity, UUID> {
+
+    List<DatabaseEntity> findByUserId (UUID useId);
+
+    DatabaseEntity findByContainerId (String id);
+
+    boolean existsByPortAndStatus (Integer port, DatabaseStatus status);
 }
